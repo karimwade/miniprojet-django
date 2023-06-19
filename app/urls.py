4f195fd300dd67db9045d2d53b1e0a6aa2b04f26
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-06-19 13:16:14 +0000

    ajout et configuration  urls dans app

---
+++ app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from app import views
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('about',views.about,name="about")
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+#about view
+def about(request):
+    return render(request,"about.html")
