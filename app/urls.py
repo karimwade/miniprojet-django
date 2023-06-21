3f2ee226b59c1782e7652291183571f2b290030b
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-06-21 15:23:31 +0000

    creation du superUser pour l'administration

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('',views.index,name="index"),
+    path('insert',views.inserData,name="insert"),
     path('about',views.about,name="about")
 ]
