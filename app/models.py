b1f9efa05330003089bb371aa6b920fe10215c13
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-06-21 14:51:38 +0000

    creation du model student

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=25,blank=False,null=False)
+    mail=models.EmailField()
+    gender=models.CharField(max_length=25,blank=False,null=False)
+
+    def __str__(self) :
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -7,3 +7,13 @@ def index(request):
 #about view
 def about(request):
     return render(request,"about.html")
+
+#insert data view
+def inserData(request):
+    if request.method == "POST":
+        name=request.POST.get("name")
+        mail=request.POST.get("mail")
+        gender=request.POST.get("gender")
+        print(name,mail,gender)
+        #query=Student(name=name)
+    return render(request,"index.html")
