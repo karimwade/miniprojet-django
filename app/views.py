cb89c08eca60ea267fabc6a787efa21c57dbed29
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-06-23 15:20:39 +0000

    Ajout des fonctionallites d'alerte d'ajout de modification et de suppresion

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Student
+from django.contrib import messages
 # Create your views here.
 def index(request):
     #load data on the template
@@ -22,6 +23,7 @@ def inserData(request):
         print(name,mail,gender)
         query=Student(name=name,mail=mail,gender=gender)
         query.save()
+        messages.info(request,"Data save succesfully")
         return redirect("/")
     return render(request,"index.html",context)
 
@@ -36,6 +38,7 @@ def updateData(request,id):
         dedit.mail=mail
         dedit.gender=gender
         dedit.save()
+        messages.warning(request,"Data update succesfully")
         return redirect("/")
     #Recupere l'id du donnée à modifier
     d=Student.objects.get(id=id)
@@ -47,5 +50,5 @@ def updateData(request,id):
 def deleteData(request,id):
     data=Student.objects.get(id=id)
     data.delete()
-    # context={"data":data}
+    messages.error(request,"Data delete succesfully")
     return redirect("/")
